7c98237fdcb04b30d4b36244d0dd5ed0c32dfd28
김창환 <63185486+rlackdghks@users.noreply.github.com>
2021-01-02 15:40:16 +0900

    2021-01-02 Intial Commit

---
+++ src/main/java/HelloController.java
@@ -0,0 +1,8 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/api/person")
+@RestController
+public class HelloController {
+
+}
